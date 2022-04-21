34fce480a1dcac16e8b3372348cf410e2ee97037
Carl <xiaoliu911@126.com>
2022-04-21 19:19:21 +0800

    优化

--- testTk.py
+++ testTk.py
@@ -1,25 +1,212 @@
 # -*- coding:utf-8 -*-
 import os
-import sys
-sys.path.append(os.path.abspath("."))
-import time
-import tkinter
+import subprocess
+import threading
+import datetime
 import socket
 import qrcode
+from tkinter import *
 import platform
-from tkinter import messagebox
+from PIL import Image as Img
+from PIL import ImageTk
 from tkinter import ttk
-import subprocess
-import datetime
 from tkinter.filedialog import *
-from PIL import ImageTk, Image
+from tkinter import messagebox
+import time
 
 
-class mobileTools(object):
+class App():
+
+    def __init__(self):
+        '''初始化页面'''
+        frame = Frame(window)
+        frame.pack()
+        self.tab()
+        self.status()
+        self.device_log()
+        self.get_fileBtn()
+        self.install_package_btn()
+        self.uninstall_wallet()
+        self.screen_shot()
+        self.show_pc_host()
+        self.share_screen_btn()
+        self.show_qrcode_btn()
+        self.deviceConnect()
+
+    def tab(self):
+        '''tab栏'''
+        self.tabNode = ttk.Notebook()
+        self.deviceControl = Frame(self.tabNode)
+        self.qrControl = Frame(self.tabNode)
+        self.tabNode.add(self.deviceControl, text="手机")
+        self.tabNode.add(self.qrControl, text="二维码")
+        self.tabNode.pack(expand=1, fill='both')
+
+    def status(self):
+        '''定义手机状态'''
+        self.deviceStatus = Label(self.deviceControl, text="设备显示")
+        self.statusText = Text(self.deviceControl,  width=30,
+                               height=1.4, bd=2, fg='blue', font='Helvetica -16')
+        self.statusBtn = Button(self.deviceControl, text="更新状态", command=self.deviceConnect)
+        self.deviceStatus.place(x=10, y=11)
+        self.statusText.place(x=70, y=10)
+        self.statusBtn.place(x=360, y=4)
+
+    def device_log(self):
+        '''获取设备日志显示'''
+        self.logBtn = Button(self.deviceControl, text='获取日志', width=10, command=self.show_log_file)
+        self.logFilePath = Text(self.deviceControl, fg="blue",
+                           bd=2, width=60, height=1, font='Helvetica -16')
+        self.logBtn.place(x=10, y=40)
+        self.logFilePath.delete(1.0, END)
+        self.logFilePath.insert(1.0, "日志存放路径")
+        self.logFilePath.place(x=92, y=46)
+
+    def get_fileBtn(self):
+        '''打开文件路径'''
+        self.importFileBtn = Button(self.deviceControl, text='导入安装包', command=self.get_file_path)
+        self.importFileBtn.place(x=10, y=80)
+
+    def get_file_path(self):
+        '''获取文件路径'''
+        filepath = askopenfilename()
+        self.entry_import.delete(0, END)
+        self.entry_import.insert(0, filepath)
+
+    def install_package_btn(self):
+        '''安装apk'''
+        self.entry_import = Entry(self.deviceControl, width=60, bd=2, font=("宋体", 11))
+        self.installBt = Button(self.deviceControl, text="安装", width=13, command=self.install_package)
+        self.entry_import.delete(0, END)
+        self.entry_import.insert(0, "apk路径...")
+        self.entry_import.place(x=80, y=88)
+        self.installBt.place(x=570, y=80)
+
+    def uninstall_wallet(self):
+        '''卸载钱包'''
+        self.uninstallBtn = Button(self.deviceControl, text="卸载钱包", width=20,
+                                   command=DeviceTools().uninstall_huobi)
+        self.uninstallBtn.place(x=10, y=120)
+
+    def screen_shot(self):
+        '''手机截图显示'''
+        self.screenShotBtn = Button(self.deviceControl, text="手机截图", width=20,
+                                    command=self.show_screenshot_pic)
+        self.screenShotBtn.place(x=200, y=120)
+
+    def share_screen_btn(self):
+        '''手机屏幕共享'''
+        self.shareScreenBt = Button(self.deviceControl, text="屏幕共享", width=20,
+                                    command=DeviceTools().share_screen)
+        self.shareScreenBt.place(x=10, y=150)
+
+    def show_pc_host(self):
+        '''获取电脑ip'''
+        self.getHostIpBt = Button(self.deviceControl, text="获取ip地址", width=20,
+                                  command=self.get_host_ip)
+        self.getHostIpBt.place(x=200, y=150)
+
+    def deviceConnect(self):
+        '''设备链接'''
+        self.statusText.delete(1.0, END)
+        self.statusText.insert(1.0, DeviceTools().get_device())
+
+    def show_log_file(self):
+        '''日志路径显示'''
+        self.logFilePath.delete(1.0, END)
+        self.logFilePath.insert(1.0, DeviceTools().get_log())
+
+    def show_qrcode_btn(self):
+        '''二维码显示'''
+        self.qrInfoText = Text(self.qrControl, width=57, height=4, fg='black', bd=2, font='Helvetica -16')
+        self.qrInfoBt = Button(self.qrControl, text='生成二维码', width=30,
+                               command=self.show_qr_img)
+        self.qrInfoText.delete(1.0, END)
+        self.qrInfoText.insert(1.0, "输入生成二维码信息")
+        self.qrInfoText.place(x=10, y=10)
+        self.qrInfoBt.place(x=100, y=100)
+
+
+    def install_package(self):
+        '''安装package'''
+        file_path = self.entry_import.get()
+        print(file_path)
+        if  " " in str(file_path):
+            messagebox.showinfo(message="apk路径有空格\n安装失败")
+        else:
+            status = DeviceTools().runCmd("adb devices").strip()
+            if status == "List of devices attached":
+                return messagebox.showinfo(message="手机未链接\n请重新链接手机")
+            elif ".apk" in str(file_path):
+                p = "adb shell pm list packages"
+                if "com.huobionchainwallet" in DeviceTools().runCmd(p):
+                    DeviceTools().runCmd("adb install -r " + file_path)
+                    messagebox.showinfo(message="install success")
+                else:
+                    DeviceTools().runCmd("adb install " + file_path)
+                    if "com.huobionchainwallet" in DeviceTools().runCmd(p):
+                        messagebox.showinfo(message="安装成功")
+                    else:
+                        messagebox.showinfo(message="安装失败")
+            else:
+                messagebox.showinfo(message="确认文件是否正确")
+
+    def show_screenshot_pic(self):
+        '''显示截图'''
+        global img0
+        ctime = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        photo = Img.open(DeviceTools().screen_shot_method())
+        photo = photo.resize((250, 500))
+        img0 = ImageTk.PhotoImage(photo)
+        img1 = Label(self.deviceControl, image=img0)
+        img1.place(x=750, y=50)
+        show_path = Label(self.deviceControl,
+                                  text=os.path.abspath(".") + r'\screenshot' + ctime + ".png",
+                                  font=("微软雅黑", 10), fg="green")
+        show_path.place(x=710, y=10)
+
+    def get_host_ip(self):
+        '''获取电脑ip'''
+        _ip = socket.gethostbyname(socket.gethostname())
+        ip_label = Label(self.deviceControl, text="ip: " + _ip, font=("宋体", 14, ), fg="blue")
+        ip_label.place(x=400, y=150)
+        return ip_label
+
+    def qrcode_generation(self):
+        '''二维码生成'''
+        file_path = "/qrcodeImg"
+        DeviceTools().creat_file(file_path=file_path)
+        qr_file = os.getcwd() + file_path
+        self.qc_info = self.qrInfoText.get("1.0", END)
+        self.qr = qrcode.QRCode(
+            version=3,
+            error_correction=qrcode.constants.ERROR_CORRECT_Q,
+            box_size=6,
+            border=3,
+        )
+        self.qr.add_data(self.qc_info)
+        self.qr.make(fit=True)
+        self.qr_img = self.qr.make_image()
+        return self.qr_img.save(qr_file + "/img.png")
+
+    def show_qr_img(self):
+        """展示二维码图片"""
+        self.qrcode_generation()
+        self.img = Img.open(os.getcwd() + "/qrcodeImg/img.png")
+        self.photo = ImageTk.PhotoImage(self.img)
+        self.qc_label = Label(self.qrControl, image=self.photo)
+        self.qc_label.place(x=600, y=10)
+
+class DeviceTools(App):
+
+    def __init__(self):
+        super(App, self).__init__()
+
 
     def runCmd(self, str):
         '''启动cmd'''
-        cmd = os.popen(str)
+        self.str = str
+        cmd = os.popen(self.str)
         return cmd.read()
 
     def get_device(self):
@@ -49,41 +236,21 @@ class mobileTools(object):
         file_new = os.path.join(log_file, lists[-1])
         return file_new
 
-    def get_file_path(self):
-        '''获取文件路径'''
-        filepath = askopenfilename()
-        entry_import.delete(0, tkinter.END)
-        entry_import.insert(0, filepath)
-
-    def install_package(self):
-        '''安装package'''
-        file_path = entry_import.get()
-        if  " " in str(file_path):
-            tkinter.messagebox.showinfo(message="apk路径有空格\n安装失败")
+    def creat_file(self, file_path):
+        """判断目录是否存在，没有则创建"""
+        self._file = os.getcwd() + file_path
+        if not os.path.exists(self._file):
+            return os.mkdir(self._file)
         else:
-            status = self.runCmd("adb devices").strip()
-            if status == "List of devices attached":
-                return tkinter.messagebox.showinfo(message="手机未链接\n请重新链接手机")
-            elif ".apk" in str(file_path):
-                p = "adb shell pm list packages"
-                if "com.huobionchainwallet" in self.runCmd(p):
-                    self.runCmd("adb install -r " + file_path)
-                    messagebox.showinfo(message="install success")
-                else:
-                    self.runCmd("adb install " + file_path)
-                    if "com.huobionchainwallet" in self.runCmd(p):
-                        messagebox.showinfo(message="安装成功")
-                    else:
-                        messagebox.showinfo(message="安装失败")
-            else:
-                tkinter.messagebox.showinfo(message="确认文件是否正确")
+            pass
 
-    def uninstall_package(self):
+
+    def uninstall_huobi(self):
         '''卸载安装包'''
         status = self.runCmd("adb devices").strip()
         str = "adb uninstall com.huobionchainwallet"
         if status == "List of devices attached":
-            return tkinter.messagebox.showinfo(message="手机未链接\n请重新链接手机")
+            return messagebox.showinfo(message="手机未链接\n请重新链接手机")
         else:
             self.runCmd(str)
             if "com.huobionchainwallet" in self.runCmd(str):
@@ -91,7 +258,7 @@ class mobileTools(object):
             else:
                 messagebox.showinfo(message="卸载成功")
 
-    def screen_Shot(self):
+    def screen_shot_method(self):
         """截图"""
         _file = "/screenShot"
         self.creat_file(file_path=_file)
@@ -101,7 +268,7 @@ class mobileTools(object):
         str = "adb shell screencap -p /sdcard/" +  ctime + ".png"
         pull_str = "adb pull /sdcard/" + ctime + ".png" + " " + scr_file
         if status == "List of devices attached":
-            return tkinter.messagebox.showinfo(message="手机未链接\n请重新链接手机")
+            return messagebox.showinfo(message="手机未链接\n请重新链接手机")
         else:
             self.runCmd(str)
             self.runCmd(pull_str)
@@ -109,61 +276,6 @@ class mobileTools(object):
             lists.sort(key=lambda fn: os.path.getmtime(scr_file + '/' + fn))
             file_new = os.path.join(scr_file, lists[-1])
             return file_new
-
-    def show_packages_3(self):
-        '''查看三方库'''
-        pass
-
-    def get_host_ip(self):
-        '''获取电脑ip'''
-        _ip = socket.gethostbyname(socket.gethostname())
-        ip_label = tkinter.Label(deviceControl, text="ip: " + _ip, font=("宋体", 14, ), fg="blue")
-        ip_label.place(x=400, y=150)
-        return ip_label
-
-    def get_hash_md5(self):
-        '''md5加密'''
-        pass
-
-    def creat_file(self, file_path):
-        """
-        判断目录是否存在，不存在则创建
-        :param file_path:
-        :return:
-        """
-        self._file = os.getcwd() + file_path
-        if not os.path.exists(self._file):
-            return os.mkdir(self._file)
-        else:
-            pass
-
-    def qrcode_generation(self):
-        '''二维码生成'''
-        file_path = "/qrcodeImg"
-        self.creat_file(file_path=file_path)
-        qr_file = os.getcwd() + file_path
-        self.qc_info = qrInfoText.get("1.0", tkinter.END)
-        self.qr = qrcode.QRCode(
-            version=3,
-            error_correction=qrcode.constants.ERROR_CORRECT_Q,
-            box_size=6,
-            border=3,
-        )
-        self.qr.add_data(self.qc_info)
-        self.qr.make(fit=True)
-        self.qr_img = self.qr.make_image()
-        return self.qr_img.save(qr_file + "/img.png")
-
-    def show_qr_img(self):
-        """
-        展示二维码图片
-        :return:
-        """
-        self.qrcode_generation()
-        self.img = Image.open(os.getcwd() + "/qrcodeImg/img.png")
-        self.photo = ImageTk.PhotoImage(self.img)
-        self.qc_label = tkinter.Label(qrControl, image=self.photo)
-        self.qc_label.place(x=600, y=10)
 
     def share_screen(self):
         '''手机同屏显示'''
@@ -174,104 +286,12 @@ class mobileTools(object):
         else:
             os.system("/usr/local/bin/scrcpy")
 
-    def show_screenshot_pic(self):
-        '''显示截图'''
-        global img0
-        ctime = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        photo = Image.open(mobileTools().screen_Shot())
-        photo = photo.resize((250, 500))
-        img0 = ImageTk.PhotoImage(photo)
-        img1 = tkinter.Label(deviceControl, image=img0)
-        img1.place(x=750, y=50)
-        show_path = tkinter.Label(deviceControl,
-                                  text=os.path.abspath(".") + r'\screenshot' + ctime + ".png",
-                                  font=("微软雅黑", 10), fg="green")
-        show_path.place(x=710, y=10)
 
-
-    def get_record_file(self):
-        '''取出录屏文件'''
-        self.stop_task()
-        _file = "/Record"
-        self.creat_file(file_path=_file)
-        file_path = os.getcwd()
-        _file = file_path + r'\Record'
-        if os.path.exists(_file):
-            pass
-        else:
-            os.mkdir(_file)
-        str = "adb pull " + self.record_file + _file
-        get_file = self.runCmd(str)
-        return get_file
-
-def deviceConnect():
-    '''设备链接状态文案'''
-    deviceStatusText.delete(1.0, tkinter.END)
-    deviceStatusText.insert(1.0, mobileTools().get_device())
-def show_log_file():
-    '''显示日志路径'''
-    deviceLogPath.delete(1.0, tkinter.END)
-    deviceLogPath.insert(1.0, mobileTools().get_log())
-
-
-
-#创建主窗口
-window = tkinter.Tk()
-window.title("Android 测试工具")
-window.geometry("1100x650+10+10")
-#Frame
-tabNote = ttk.Notebook()
-deviceControl = ttk.Frame(tabNote) #常用
-qrControl = ttk.Frame(tabNote) # 二维码
-tabNote.add(deviceControl, text="常用")
-tabNote.add(qrControl, text="二维码")
-tabNote.pack(expand=1, fill='both')
-
-#设备状态
-deviceStatus = tkinter.Label(deviceControl, text='设备状态')
-deviceStatusText = tkinter.Text(deviceControl, width=30, height=1.4, bd=2, fg='blue', font='Helvetica -16')
-deviceConnect()
-refreshStatusBtn = tkinter.Button(deviceControl, text="更新状态", command=deviceConnect)
-deviceStatus.place(x=10, y=11)
-deviceStatusText.place(x=70, y=10)
-refreshStatusBtn.place(x=360, y=4)
-#日志部分
-deviceLogBt = tkinter.Button(deviceControl,  text='获取日志', width=10, command=show_log_file)
-deviceLogPath =tkinter.Text(deviceControl, fg="blue",
-                           bd=2, width=60, height=1, font='Helvetica -16')
-deviceLogBt.place(x=10, y=40)
-deviceLogPath.delete(1.0, tkinter.END)
-deviceLogPath.insert(1.0, "日志存放路径")
-deviceLogPath.place(x=92, y=46)
-#安装卸载
-importFileBt = tkinter.Button(deviceControl, text='导入安装包', command=mobileTools().get_file_path)
-entry_import = tkinter.Entry(deviceControl, width=60, bd=2, font=("宋体", 11, 'bold'))
-installBt = tkinter.Button(deviceControl, text="安装", width=13, command=mobileTools().install_package)
-uninstallBt = tkinter.Button(deviceControl, text="卸载钱包", width=20, command=mobileTools().uninstall_package)
-entry_import.delete(0, tkinter.END)
-entry_import.insert(0, "apk路径...")
-importFileBt.place(x=10, y=80)
-entry_import.place(x=80, y=88)
-installBt.place(x=570, y=80)
-uninstallBt.place(x=10, y=120)
-#截图
-screenShotBt = tkinter.Button(deviceControl, text="截图", width=20, command=mobileTools().show_screenshot_pic)
-screenShotBt.place(x=200, y=120)
-#实时屏幕
-shareScreenBt = tkinter.Button(deviceControl, text="屏幕共享",width=20, command=mobileTools().share_screen)
-shareScreenBt.place(x=10, y=150)
-#获取ip
-getHostIpBt = tkinter.Button(deviceControl, text="获取ip地址", width=20, command=mobileTools().get_host_ip)
-getHostIpBt.place(x=200, y=150)
-
-#二维码显示
-qrInfoText = tkinter.Text(qrControl, width=57, height=4, fg='black', bd=2, font='Helvetica -16')
-qrInfoBt = tkinter.Button(qrControl, text='生成二维码', width=30, command=mobileTools().show_qr_img)
-qrInfoText.delete(1.0, tkinter.END)
-qrInfoText.insert(1.0, "输入生成二维码信息")
-qrInfoText.place(x=10, y=10)
-qrInfoBt.place(x=100, y=100)
 
 
 if __name__ == '__main__':
+    window = Tk()
+    window.title("测试")
+    window.geometry("1100x650")
+    app = App()
     window.mainloop()
