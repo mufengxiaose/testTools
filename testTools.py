7b5139cdebc05b058c6784fcce2a68aceb9b8aad
liuxiao <cliuxiao_v@didichuxing.com>
2022-08-26 16:56:39 +0800

    重新封装手机功能

--- testTools.py
+++ testTools.py
@@ -11,8 +11,11 @@ import os, time, sys
 import qrcode
 import requests
 import platform
+import subprocess
+import datetime
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from PIL import ImageTk
 from PIL import Image as Img
 
@@ -105,6 +108,7 @@ class DevicesApp(tk.Frame):
         tk.Frame.__init__(self, master)
         self.pack()
 
+
         self.deviceFrame = tk.Frame(self)
         self.deviceFrame.grid()
         # 手机链接状态，设备名显示
@@ -114,7 +118,8 @@ class DevicesApp(tk.Frame):
         self.status_text = tk.Text(self.deviceFrame, width=20, height=1, font=20)
         self.status_text.grid(row=0, column=1, sticky=tk.W)
 
-        self.refresh_status_bt = tk.Button(self.deviceFrame, text="刷新状态", width=12)
+        self.refresh_status_bt = tk.Button(self.deviceFrame, text="刷新状态", width=12,
+                                           command=self.deviceConnect)
         self.refresh_status_bt.grid(row=0, column=2)
 
         # 获取手机日志
@@ -124,22 +129,105 @@ class DevicesApp(tk.Frame):
         self.log_text = tk.Entry(self.deviceFrame, width=60)
         self.log_text.grid(row=1, column=1, sticky=tk.W)
 
-        self.get_log_bt = tk.Button(self.deviceFrame, width=12, text="获取手机日志")
+        self.get_log_bt = tk.Button(self.deviceFrame, width=12, text="获取手机日志",
+                                    command=self.showLogPath)
         self.get_log_bt.grid(row=1, column=2, sticky=tk.W)
 
         # Android屏幕共享
-        self.scrcpy_bt = tk.Button(self.deviceFrame, text="投屏", width=12)
+        self.scrcpy_bt = tk.Button(self.deviceFrame, text="投屏", width=12, command=self.callScrcpy)
         self.scrcpy_bt.grid(row=2, column=0, sticky=tk.W)
 
         # Android截图
         self.screenshot_bt = tk.Button(self.deviceFrame, text="手机截图", width=12,
-                                       command=self.callScrcpy)
+                                       command=self.creatScreenshotToplevel)
         self.screenshot_bt.grid(row=2, column=1, sticky=tk.W)
 
+        # 启动app直接获取设备链接状态
+        self.deviceConnect()
+
+    #手机状态部分
     def callScrcpy(self):
         '''使用scrcpy功能'''
-        system = CommonFunc().getSystemName()
-        print(system)
+        if CommonFunc().getSystemName() == 'Window':
+            pass
+        else:
+            return os.popen('scrcpy')
+
+    def GetDeviceList(self):
+        '''获取设备状态'''
+        status = CommonFunc().runCmd("adb devices").strip()
+        if status == "List of devices attached":
+            status = "设备链接失败"
+        elif "offline" in status:
+            subprocess.Popen("adb kill-server")
+            subprocess.Popen("adb devices")
+        else:
+            status = status.replace("List of devices attached", "").strip()
+        return status
+
+    def deviceConnect(self):
+        '''设备链接'''
+        self.status_text.delete(1.0, tk.END)
+        self.status_text.insert(1.0, self.GetDeviceList())
+
+    #log 部分
+    def GetLog(self):
+        '''获取设备日志'''
+        _file = '/mobile_log'
+        CommonFunc().creatFile(file_path=_file)
+        log_file = os.getcwd() + _file
+        ctime = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        file = "adb logcat -v threadtime > " + log_file + "/" + ctime + ".log"
+        log1 = subprocess.Popen(args=file, shell=True, stdin=subprocess.PIPE, stdout=None)
+        time.sleep(15)
+        os.system("taskkill /t /f /pid {}".format(log1.pid))
+        lists = os.listdir(log_file)
+        lists.sort(key=lambda fn:os.path.getmtime(log_file + '/' + fn))
+        file_new = os.path.join(log_file, lists[-1])
+        return file_new
+
+    def showLogPath(self):
+        '''日志路径显示'''
+        self.log_text.delete(1, tk.END)
+        self.log_text.insert(1, self.GetLog())
+
+    #Android手机截图
+    def screenshotMethod(self):
+        """截图"""
+        _file = "/screenShot"
+        CommonFunc().creatFile(file_path=_file)
+        scr_file = os.getcwd() + _file
+        ctime = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        status = CommonFunc().runCmd("adb devices").strip()
+        str = "adb shell screencap -p /sdcard/" +  ctime + ".png"
+        pull_str = "adb pull /sdcard/" + ctime + ".png" + " " + scr_file
+        if status == "List of devices attached":
+            return messagebox.showinfo(message="手机未链接\n请重新链接手机")
+        else:
+            CommonFunc().runCmd(str)
+            CommonFunc().runCmd(pull_str)
+            lists = os.listdir(scr_file)
+            lists.sort(key=lambda fn: os.path.getmtime(scr_file + '/' + fn))
+            file_new = os.path.join(scr_file, lists[-1])
+            return file_new
+
+    def showScreenshotPic(self):
+        '''显示截图'''
+        global screenImg
+        photo = Img.open(self.screenshotMethod())
+        width, height = photo.size[0], photo.size[1] #获取图片宽高
+        photo = photo.resize((int(width*0.3), int(height*0.3))) #图盘等比缩放
+        screenImg = ImageTk.PhotoImage(photo)
+        return screenImg
+
+    def creatScreenshotToplevel(self):
+        '''创建toplevel'''
+        top = tk.Toplevel()
+        top.title("截图")
+        top.geometry('320x630')
+        label = tk.Label(top, image=self.showScreenshotPic())
+        label.pack()
+
 
 
 class CommonFunc():
@@ -165,7 +253,7 @@ class CommonFunc():
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("test tools")
-    root.geometry("800x600+10+10")
+    root.geometry("1000x600+10+10")
 
     tabNote = ttk.Notebook(root)
     tabNote.add(DevicesApp(tabNote), text="手机常用功能")
