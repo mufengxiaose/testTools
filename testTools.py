22faf582e5bca09c9ffdf2e120a08a5bb2e4d789
liuxiao <cliuxiao_v@didichuxing.com>
2022-08-27 17:41:51 +0800

    增加百度翻译

--- testTools.py
+++ testTools.py
@@ -13,6 +13,8 @@ import requests
 import platform
 import subprocess
 import datetime
+import hashlib
+import random
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
@@ -25,20 +27,33 @@ class TranslaterApp(tk.Frame):
     '''翻译功能'''
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self.place(width=800, height=600)
+        self.pack()
         # 设置frame
-        text_frame = tk.Frame(self, borderwidth=1, relief='sunken')
-        text_frame.pack(padx=10, pady=10)
+        text_frame = tk.Frame(self, borderwidth=1, relief='sunken',width=500, height=600)
+        text_frame.pack()
+
+        data = ['英译汉','汉英译']
+        self.combobox = ttk.Combobox(text_frame)
+        self.combobox['value'] = data
+        self.combobox.current(0)
+        self.combobox.grid(row=0, column=1, sticky=tk.W)
+
         # 文字输入
-        self.input_text = tk.Text(text_frame, width=300, height=10, wrap=tk.WORD, font=15)
-        self.input_text.pack()
-        # 翻译按钮
-        self.button = tk.Button(text_frame, text="翻译", command=self.resultsText,
-                                width=30)
-        self.button.pack(anchor="center")
+        label0 = tk.Label(text_frame, text="待翻译文字：")
+        label0.grid(row=1, column=0, sticky=tk.W)
+        self.input_text = tk.Text(text_frame, height=8)
+        self.input_text.grid(row=1, column=1, columnspan=3)
+        # 有道翻译按钮
+        self.button = tk.Button(text_frame, text="有道翻译", command=self.resultsText)
+        self.button.grid(row=2, column=1, sticky=tk.W)
+        #百度翻译
+        self.baiduApi_bt = tk.Button(text_frame, text="百度翻译", command=self.resultsText)
+        self.baiduApi_bt.grid(row=2, column=2, sticky=tk.W)
         # 显示翻译结果
-        self.results_text = tk.Text(text_frame, width=300, height=10, wrap=tk.WORD)
-        self.results_text.pack()
+        label1 = tk.Label(text_frame, text='翻译结果：')
+        label1.grid(row=3, column=0, sticky=tk.W)
+        self.results_text = tk.Text(text_frame, wrap=tk.WORD, height=8)
+        self.results_text.grid(row=3, column=1, sticky=tk.W, columnspan=3)
 
     def getInputText(self):
         '''获取输入信息'''
@@ -47,7 +62,10 @@ class TranslaterApp(tk.Frame):
     def resultsText(self):
         '''显示翻译结果'''
         self.results_text.delete(1.0, tk.END)
-        self.results_text.insert(1.0, self.getTranslate())
+        try:
+            self.results_text.insert(1.0, self.getTranslate())
+        except:
+            self.results_text.insert(1.0, self.baiduApi())
 
     def getTranslate(self):
         '''获取有道翻译结果'''
@@ -59,6 +77,42 @@ class TranslaterApp(tk.Frame):
         }
         r = requests.get(url=youdao_url, params=params)
         return r.json()['translateResult'][0][0]['tgt']
+    
+    # 百度翻译api
+    def getMd5(self, data):
+        '''md5加密'''
+        return hashlib.md5(str(data).encode(encoding='utf-8')).hexdigest()
+
+    def baiduApi(self):
+        '''百度api调用'''
+        url = "https://fanyi-api.baidu.com/api/trans/vip/translate"
+        appid = "20220813001305350"
+        key = "6096HbMy1F6htfIXEJzk"
+        salt = random.randint(300000,900000)
+        sign_value = self.getMd5(appid + self.getInputText() + str(salt) + key)
+        if self.combobox.get() == "英译汉":
+            parmas = {
+                "q": self.getInputText(),
+                "from": "auto",
+                "to": "zh",
+                "appid": appid,
+                "salt": salt,
+                "sign":str(sign_value),
+            }
+            r = requests.get(url=url, params=parmas)
+            return r.json()['trans_result'][0]['dst']
+        else:
+            parmas = {
+                "q": self.getInputText(),
+                "from": "auto",
+                "to": "en",
+                "appid": appid,
+                "salt": salt,
+                "sign": str(sign_value),
+            }
+            r = requests.get(url=url, params=parmas)
+            return r.json()['trans_result'][0]['dst']
+
 
 class QrcodeApp(tk.Frame):
     '''二维码生成'''
@@ -259,7 +313,7 @@ if __name__ == '__main__':
     tabNote.add(DevicesApp(tabNote), text="手机常用功能")
     tabNote.add(TranslaterApp(tabNote), text="翻译")
     tabNote.add(QrcodeApp(tabNote), text="二维码生成")
-
     tabNote.pack(expand=0, anchor='nw')
+
 
     root.mainloop()
