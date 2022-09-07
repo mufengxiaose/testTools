6eee3fd6a9debf68e2ab715cc24597593381eebd
liuxiao <cliuxiao_v@didichuxing.com>
2022-09-07 17:27:15 +0800

    删除冗余代码，添加安装apk功能

--- testTools.py
+++ testTools.py
@@ -7,7 +7,8 @@
 # Author     ：Carl
 # Description：
 """
-import os, time, sys
+import os
+import time
 import qrcode
 import requests
 import platform
@@ -15,53 +16,54 @@ import subprocess
 import datetime
 import hashlib
 import random
-import tkinter as tk
+import threading
+from tkinter.filedialog import *
+from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from PIL import ImageTk
 from PIL import Image as Img
 
 
-
-class TranslaterApp(tk.Frame):
+class TranslaterApp(Frame):
     '''翻译功能'''
     def __init__(self, master):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.pack()
         # 设置frame
-        text_frame = tk.Frame(self, borderwidth=1, relief='sunken',width=500, height=600)
+        text_frame = Frame(self, borderwidth=1, relief='sunken',width=500, height=600)
         text_frame.pack()
 
         data = ['英译汉','汉英译']
         self.combobox = ttk.Combobox(text_frame)
         self.combobox['value'] = data
         self.combobox.current(0)
-        self.combobox.grid(row=0, column=1, sticky=tk.W)
+        self.combobox.grid(row=0, column=1, sticky=W)
 
         # 文字输入
-        label0 = tk.Label(text_frame, text="待翻译文字：")
-        label0.grid(row=1, column=0, sticky=tk.W)
-        self.input_text = tk.Text(text_frame, height=8)
+        label0 = Label(text_frame, text="待翻译文字：")
+        label0.grid(row=1, column=0, sticky=W)
+        self.input_text = Text(text_frame, height=8)
         self.input_text.grid(row=1, column=1, columnspan=3)
         # 有道翻译按钮
-        self.button = tk.Button(text_frame, text="有道翻译", command=self.resultsText)
-        self.button.grid(row=2, column=1, sticky=tk.W)
+        self.button = Button(text_frame, text="有道翻译", command=self.resultsText)
+        self.button.grid(row=2, column=1, sticky=W)
         #百度翻译
-        self.baiduApi_bt = tk.Button(text_frame, text="百度翻译", command=self.resultsText)
-        self.baiduApi_bt.grid(row=2, column=2, sticky=tk.W)
+        self.baiduApi_bt = Button(text_frame, text="百度翻译", command=self.resultsText)
+        self.baiduApi_bt.grid(row=2, column=2, sticky=W)
         # 显示翻译结果
-        label1 = tk.Label(text_frame, text='翻译结果：')
-        label1.grid(row=3, column=0, sticky=tk.W)
-        self.results_text = tk.Text(text_frame, wrap=tk.WORD, height=8)
-        self.results_text.grid(row=3, column=1, sticky=tk.W, columnspan=3)
+        label1 = Label(text_frame, text='翻译结果：')
+        label1.grid(row=3, column=0, sticky=W)
+        self.results_text = Text(text_frame, wrap=WORD, height=8)
+        self.results_text.grid(row=3, column=1, sticky=W, columnspan=3)
 
     def getInputText(self):
         '''获取输入信息'''
-        return self.input_text.get(1.0, tk.END)
+        return self.input_text.get(1.0, END)
 
     def resultsText(self):
         '''显示翻译结果'''
-        self.results_text.delete(1.0, tk.END)
+        self.results_text.delete(1.0, END)
         try:
             self.results_text.insert(1.0, self.getTranslate())
         except:
@@ -114,24 +116,24 @@ class TranslaterApp(tk.Frame):
             return r.json()['trans_result'][0]['dst']
 
 
-class QrcodeApp(tk.Frame):
+class QrcodeApp(Frame):
     '''二维码生成'''
     def __init__(self, master):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.pack()
         # 设置frame
-        self.qrcodeFrame = tk.Frame(self)
+        self.qrcodeFrame = Frame(self)
         self.qrcodeFrame.grid()
         # 输入文字
-        self.input_text = tk.Text(self.qrcodeFrame, height=6, width=100)
+        self.input_text = Text(self.qrcodeFrame, height=6, width=100)
         self.input_text.grid(row=0)
         # 生成二维码按钮
-        self.button = tk.Button(self.qrcodeFrame, text="生成二维码", command=self.showQrcodeImg)
-        self.button.grid(row=1, sticky=tk.W)
+        self.button = Button(self.qrcodeFrame, text="生成二维码", command=self.showQrcodeImg)
+        self.button.grid(row=1, sticky=W)
 
     def getText(self):
         '''获取文字内容'''
-        return self.input_text.get("1.0", tk.END)
+        return self.input_text.get("1.0", END)
 
     def qrcodeGeneration(self):
         '''生成二维码'''
@@ -153,51 +155,58 @@ class QrcodeApp(tk.Frame):
         self.qrcodeGeneration()
         self.img = Img.open(os.getcwd() + "/qrcodeImg/img.png")
         self.photo = ImageTk.PhotoImage(self.img)
-        self.qc_label = tk.Label(self.qrcodeFrame, image=self.photo)
+        self.qc_label = Label(self.qrcodeFrame, image=self.photo)
         self.qc_label.grid(row=2)
 
-class DevicesApp(tk.Frame):
+class DevicesApp(Frame):
     '''手机部分'''
     def __init__(self, master):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.pack()
 
-
-        self.deviceFrame = tk.Frame(self)
+        self.deviceFrame = Frame(self)
         self.deviceFrame.grid()
         # 手机链接状态，设备名显示
-        self.status_label = tk.Label(self.deviceFrame, text="设备链接状态：")
-        self.status_label.grid(row=0, column=0)
+        self.status_label = Label(self.deviceFrame, text="设备链接状态：")
+        self.status_label.grid(row=0, column=0, sticky=W)
 
-        self.status_text = tk.Text(self.deviceFrame, width=20, height=1, font=20)
-        self.status_text.grid(row=0, column=1, sticky=tk.W)
+        self.status_text = Text(self.deviceFrame, width=20, height=1, font=20)
+        self.status_text.grid(row=0, column=1, sticky=W, columnspan=3)
 
-        self.refresh_status_bt = tk.Button(self.deviceFrame, text="刷新状态", width=12,
+        self.refresh_status_bt = Button(self.deviceFrame, text="刷新状态", width=12,
                                            command=self.deviceConnect)
-        self.refresh_status_bt.grid(row=0, column=2)
+        self.refresh_status_bt.grid(row=0, column=4)
 
         # 获取手机日志
-        self.log_label = tk.Label(self.deviceFrame, text="日志存放路径：")
-        self.log_label.grid(row=1, column=0, sticky=tk.W)
+        self.log_label = Label(self.deviceFrame, text="日志存放路径：")
+        self.log_label.grid(row=1, column=0, sticky=W)
 
-        self.log_text = tk.Entry(self.deviceFrame, width=60)
-        self.log_text.grid(row=1, column=1, sticky=tk.W)
+        self.log_text = Entry(self.deviceFrame, width=40)
+        self.log_text.grid(row=1, column=1, sticky=W, columnspan=3)
 
-        self.get_log_bt = tk.Button(self.deviceFrame, width=12, text="获取手机日志",
+        self.get_log_bt = Button(self.deviceFrame, width=12, text="获取手机日志",
                                     command=self.showLogPath)
-        self.get_log_bt.grid(row=1, column=2, sticky=tk.W)
+        self.get_log_bt.grid(row=1, column=4, sticky=W)
 
         # Android屏幕共享
-        self.scrcpy_bt = tk.Button(self.deviceFrame, text="投屏", width=12, command=self.callScrcpy)
-        self.scrcpy_bt.grid(row=2, column=0, sticky=tk.W)
+        self.scrcpy_bt = Button(self.deviceFrame, text="投屏", width=12, command=self.callScrcpy)
+        self.scrcpy_bt.grid(row=3, column=0, sticky=W)
 
         # Android截图
-        self.screenshot_bt = tk.Button(self.deviceFrame, text="手机截图", width=12,
+        self.screenshot_bt = Button(self.deviceFrame, text="手机截图", width=12,
                                        command=self.creatScreenshotToplevel)
-        self.screenshot_bt.grid(row=2, column=1, sticky=tk.W)
+        self.screenshot_bt.grid(row=3, column=1, sticky=W)
+
+        # 重启手机
+        self.reset_devices_bt = Button(self.deviceFrame, text='重启手机', width=12,
+                                          command=self.resetDevices)
+        self.reset_devices_bt.grid(row=3, column=2, sticky=W)
 
         # 启动app直接获取设备链接状态
         self.deviceConnect()
+
+        # 安装apk
+        self.install_apk()
 
     #手机状态部分
     def callScrcpy(self):
@@ -221,7 +230,7 @@ class DevicesApp(tk.Frame):
 
     def deviceConnect(self):
         '''设备链接'''
-        self.status_text.delete(1.0, tk.END)
+        self.status_text.delete(1.0, END)
         self.status_text.insert(1.0, self.GetDeviceList())
 
     #log 部分
@@ -242,7 +251,7 @@ class DevicesApp(tk.Frame):
 
     def showLogPath(self):
         '''日志路径显示'''
-        self.log_text.delete(1, tk.END)
+        self.log_text.delete(1, END)
         self.log_text.insert(1, self.GetLog())
 
     #Android手机截图
@@ -276,11 +285,55 @@ class DevicesApp(tk.Frame):
 
     def creatScreenshotToplevel(self):
         '''创建toplevel'''
-        top = tk.Toplevel()
+        top = Toplevel()
         top.title("截图")
         top.geometry('320x630')
-        label = tk.Label(top, image=self.showScreenshotPic())
+        label = Label(top, image=self.showScreenshotPic())
         label.pack()
+    # 重启手机
+    def resetDevices(self):
+        str0 = "adb reboot"
+        return CommonFunc().runCmd(str0)
+
+    def install_apk(self):
+        '''安装apk'''
+        openFIleBt = Button(self.deviceFrame, text="导入安装包", command=self.get_file_path)
+        openFIleBt.grid(row=2, column=0, sticky=W)
+
+        self.fileEntry = Entry(self.deviceFrame)
+        self.fileEntry.grid(row=2, column=1, columnspan=3, sticky=NSEW)
+
+        installBt = Button(self.deviceFrame, text="安装", command=self.install_thread)
+        installBt.grid(row=2, column=4, sticky=NSEW)
+
+    def get_file_path(self):
+        '''获取文件路径'''
+        filepath = askopenfilename()
+        self.fileEntry.delete(0, END)
+        self.fileEntry.insert(0, filepath)
+
+    def install_package(self):
+        '''安装package'''
+        file_path = self.fileEntry.get()
+        if " " in str(file_path):
+            messagebox.showinfo(message="apk路径有空格\n安装失败")
+        else:
+            status = CommonFunc().runCmd("adb devices").strip()
+            if status == "List of devices attached":
+                return messagebox.showinfo(message="手机未链接\n请重新链接手机")
+            elif ".apk" in str(file_path):
+                p = "adb install "
+                if "Success" in CommonFunc().runCmd(p + file_path):
+                    messagebox.showinfo(message="安装成功")
+                else:
+                    messagebox.showinfo(message="安装失败")
+            else:
+                messagebox.showinfo(message="确认文件是否正确")
+
+    def install_thread(self):
+        """启用安装线程"""
+        t1 = threading.Thread(target=self.install_package)
+        t1.start()
 
 
 
@@ -305,7 +358,7 @@ class CommonFunc():
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
+    root = Tk()
     root.title("test tools")
     root.geometry("1000x600+10+10")
 
